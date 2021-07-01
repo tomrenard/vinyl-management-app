807cd9e83c492702a090154857b705a0d0580317
Tom Renard <tom.renard@zoho.com>
2021-07-01 20:20:15 +0200

    setting up discogs api

--- src/components/VinylsList.js
+++ src/components/VinylsList.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export default function VinylsList() {
+  useEffect(() => {
+    axios.get(`https://api.discogs.com/database/search?q=Nirvana&token=WppZaBSUhugJNmSVcJQkxmUotfLfyeufVljGcpJv`)
+      .then(res =>{
+        const data = res.data;
+        console.log(data);
+      })
+      .catch(err => console.log(err));
+  });
   return (
     <h2>List of vinyls</h2>
   );
